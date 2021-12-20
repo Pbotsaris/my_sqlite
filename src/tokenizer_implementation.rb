ad2ad87b7e623a9da445ecb73d1cb84df3696fb4
Pbotsaris <pedro@antfood.com>
2021-12-20 23:03:49 +0100

    added unit test

---
+++ src/tokenizer_implementation.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# private implementation for the Tokenizer class
+module TokenizerImplementation
+  SPEC = [
+
+    [/^\s+/, nil], # whitespace
+    [/--.*/, nil], # comments
+    [/^\n/, nil], # linebreak
+    [%r{^/\*[\s\S]*?\*/}, nil], # multiline comments
+    [/^\d+/, 'NUMBER'],
+    [/^"[^"]*/, 'STRING'],
+    [/^'[^']*/, 'STRING']
+
+  ].freeze
+
+  def match(regex, line)
+    matched = line.match(regex)
+    return nil unless matched
+
+    @cursor += matched[0].length
+    matched[0]
+  end
+end
