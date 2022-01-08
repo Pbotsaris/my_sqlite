f3515e3dde74c3c680bd6524e5764d4c27db9a31
Pbotsaris <pedro@antfood.com>
2022-01-08 20:46:57 +0100

    added parsers to entrypoint class

--- src/my_sqlite.rb
+++ src/my_sqlite.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 require 'readline'
+require 'pp'
+require_relative './parser'
 
 # a class
 class SQlite
+  def initialize
+    @parser = Parser.new
+    @ast = {}
+  end
+
   def run
     while (line = Readline.readline('>', true))
       @line = line
+      @ast = @parser.parse(line)
+      pp @ast
     end
   end
 end
 
-# SQlite.new.run
+SQlite.new.run
